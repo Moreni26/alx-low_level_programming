403949f85e41580eb05cde86dd1a959e73f1687d
Moreni26 <ayomide2605@gmail.com>
2023-06-21 14:56:32 -0700

    Addition of integer of the same result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add -A function adds two integers and returns the result
+ * @a: This is  an integer
+ * @b: This is also an integer
+ * Return: sum of integer added
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
