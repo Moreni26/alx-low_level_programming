2357dcf690076064d263679fafda44f4258dd8c5
Moreni26 <ayomide2605@gmail.com>
2023-06-22 15:44:54 -0700

    printing fizzbuzz

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - Draw a straight line
+ * @n: The number of time the character should be printed.
+ * Description: This function draw a straight line in the terminal.
+ * Return: Always 0 (Success).
+ */
+void print_line(int n)
+{
+	int n;
+
+	for ( 
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This function print number from 1 to 100
+ * on a new line.
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FzzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
