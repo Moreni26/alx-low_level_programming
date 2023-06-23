bbd18de945742100765f536f59d784bcab763389
Moreni26 <ayomide2605@gmail.com>
2023-06-23 10:38:17 -0700

    Debugging is stressful

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * main - Entry point
- * Description: This function prints if integer is positive or
- * negative.
- * Return: void
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
