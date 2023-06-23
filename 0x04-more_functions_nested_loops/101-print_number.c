305bb701ab30d927fa9c4514dfbb3f9a3fc9903b
Moreni26 <ayomide2605@gmail.com>
2023-06-23 16:57:38 -0700

    corrected

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,8 @@
 #include "main.h"
+
 /**
- * * print_number - Write a function that prints an integer
- * wow! I wont give up
- * @n: paramaters for integers and numbers
- * Return: empty
+ * print_number - prints an integer
+ * @n: param
  */
 void print_number(int n)
 {
@@ -18,14 +17,16 @@ void print_number(int n)
 	{
 		i = n;
 	}
+
 	j = i;
 	count = 1;
-	while (j > 9)
 
+	while (j > 9)
 	{
 		j /= 10;
 		count *= 10;
 	}
+
 	for (; count >= 1; count /= 10)
 	{
 		_putchar(((i / count) % 10) + 48);
