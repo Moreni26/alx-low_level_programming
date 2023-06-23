bcac421818e6c28fcee2efaf241ceb93297e15c0
Moreni26 <ayomide2605@gmail.com>
2023-06-23 01:43:25 -0700

    printing fizz buzz

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal - Draw a diagonal line
+ * @n: The number of time the character \ should be printed.
+ */
+void print_diagonal(int n)
+{
+	if (n = 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		int i, j;
+        	for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					putchar('\\');
+				else if (j < i)
+					putchar(' ');
+			}
+			putchar('\n');
+		}
+	}
+}
